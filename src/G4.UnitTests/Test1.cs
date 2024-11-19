4f078e5a568736fa6571debcfb9991558cfd7b69
gravity-api <gravity.api@outlook.com>
2024-11-19 10:19:20 +0200

    Testing a feature - will not be part of production
    Update release pipeline and add unit tests
    
    Updated `release-pipeline.yml` to trigger on `development` branch. Removed `invoke-api-tests` job and commented out `publish-hub-docker`, `publish-hub-standalone`, and `new-release` jobs. Added `test-test` job for unit tests on Windows. Updated `G4.Services.sln` to include `G4.UnitTests` project and `.github` folder. Added `G4.UnitTests.csproj` for MSTest targeting .NET 8.0. Added `MSTestSettings.cs` for parallel test execution. Added `Test1.cs` with a test for `CacheManager` initialization.

---
+++ src/G4.UnitTests/Test1.cs
@@ -0,0 +1,18 @@
+﻿using G4.Cache;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+
+namespace G4.UnitTests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod, TestCategory("ExportData"), ExpectedException(typeof(TypeInitializationException))]
+        public void TestMethod1()
+        {
+            var cache = CacheManager.Instance;
+        }
+    }
+}
