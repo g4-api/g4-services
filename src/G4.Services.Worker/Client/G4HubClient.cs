6ddf27994cd80a6abab4df5630cb24ef58a4e3e6
gravity-api <gravity.api@outlook.com>
2025-03-05 20:42:19 +0200

    Add G4HubClient for SignalR communication
    Introduces the `G4HubClient` class in the `G4.Services.Worker.Client` namespace. This class facilitates real-time communication with a SignalR hub, providing methods to receive automation requests and send automation status updates. A private method, `NewHubClient`, is implemented to establish the hub connection, including error handling and connection status logging.

---
+++ src/G4.Services.Worker/Client/G4HubClient.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+
+namespace G4.Services.Worker.Client
+{
+    public class G4HubClient(Uri hubUri)
+    {
+        private readonly HubConnection _hubConnection = NewHubClient(hubUri);
+
+        public void GetAutomationRequest()
+        {
+            _hubConnection.On<string>("ReceiveAutomationRequest", (request) =>
+            {
+                Console.WriteLine($"Received automation request: {request}");
+            });
+        }
+
+        public void SendAutomationStatus(string status)
+        {
+            _hubConnection.SendAsync("SendAutomationStatus", status);
+        }
+
+        private static HubConnection NewHubClient(Uri hubUri)
+        {
+            // Create the connection
+            var connection = new HubConnectionBuilder()
+                .WithUrl($"{hubUri.AbsoluteUri.TrimEnd('/')}/hub/v4/g4/orchestrator")
+                .WithAutomaticReconnect([TimeSpan.Zero, TimeSpan.FromSeconds(2), TimeSpan.FromSeconds(10)])
+                .Build();
+
+            var route = "/hub/v4/g4/orchestrator";
+
+            try
+            {
+                // Start the connection
+                connection.StartAsync().GetAwaiter().GetResult();
+                Console.WriteLine("Connected to the hub.");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Connection error: {e.Message}");
+            }
+
+            return connection;
+        }
+    }
+}
