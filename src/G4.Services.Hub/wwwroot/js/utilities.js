91ad1be5124a210d317a2ddbe578956b5ecb9c20
gravity-api <gravity.api@outlook.com>
2025-03-13 09:46:35 +0200

    Add theme toggling functionality in utilities.js
    Implement a new static method `switchMode` to toggle
    the application's theme between default and dark modes.
    The method updates the `href` of the `theme-stylesheet`
    link element based on the current theme.

--- src/G4.Services.Hub/wwwroot/js/utilities.js
+++ src/G4.Services.Hub/wwwroot/js/utilities.js
@@ -269,6 +269,36 @@
     }
 
     /**
+     * Toggles the theme of the application between the default and dark modes.
+     *
+     * This method checks the current theme by inspecting the 'href' attribute of the
+     * <link> element with the ID 'theme-stylesheet'. It then switches the theme
+     * to the opposite mode by updating the 'href' accordingly.
+     */
+    static switchMode() {
+        // Retrieve the <link> element that holds the current theme stylesheet using its ID
+        const themeStylesheet = document.getElementById('theme-stylesheet');
+
+        // Get the current stylesheet path and convert it to lowercase for comparison
+        const theme = themeStylesheet.getAttribute('href').toLowerCase();
+
+        // Define the default (blueprint) theme stylesheet path
+        const defaultTheme = './css/designer-blueprint-parameters.css';
+
+        // Define the dark theme stylesheet path
+        const darkTheme = './css/designer-blueprint-parameters-dark.css';
+
+        // Toggle the theme:
+        // If the current theme is the default theme, switch to the dark theme;
+        // otherwise, switch back to the default theme.
+        if (theme === defaultTheme) {
+            themeStylesheet.setAttribute('href', darkTheme);
+        } else {
+            themeStylesheet.setAttribute('href', defaultTheme);
+        }
+    }
+
+    /**
     * Waits for a DOM element matching the specified selector to appear within a given timeout.
     *
     * This function repeatedly checks for the presence of a DOM element that matches the provided
